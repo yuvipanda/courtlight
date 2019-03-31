1b0bb62cc522f9b9e0e0465ca3bb0b26c6826161
yuvipanda <yuvipanda@gmail.com>
2019-04-01 01:37:27 +0530

    De-duplicate judgements based on PDF's text sha too
    Just URL isn't enough, since it isn't always de-duplicated.

--- courtlight/highcourts/populate_contents.py
+++ courtlight/highcourts/populate_contents.py
@@ -25,8 +25,21 @@ async def populate_contents(session):
             text = utils.pdf_to_text(download_path)
             hash = hashlib.sha256()
             hash.update(text.encode('utf-8'))
-            judgement.text_content = text
-            judgement.text_content_hash = hash.hexdigest()
-            session.add(judgement)
+            text_content_hash = hash.hexdigest()
+
+            other_judgement = session.query(Judgement).filter_by(text_content_hash=text_content_hash).first()
+
+            if other_judgement:
+                # Another judgement already exists with this hash.
+                # So we kill our judgement, and point all our cases to that one
+                for case in judgement.cases:
+                    case.judgement = other_judgement
+                    session.add(case)
+                session.delete(judgement)
+                logging.info(f'Found duplicated judgement with hash {text_content_hash}, de-duplicating...')
+            else:
+                judgement.text_content = text
+                judgement.text_content_hash = hash.hexdigest()
+                session.add(judgement)
+                logging.info(f'{i+1} of {judgements_count} judgement content populated from {judgement.pdf_link}')
             session.commit()
-            logging.info(f'{i+1} of {judgements_count} judgement content populated from {judgement.pdf_link}')
